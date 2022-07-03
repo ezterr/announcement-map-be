f893a764846e328c4e9ea0f170fabf226a0584d8
JakubL <jakubxleszczewski@gmail.com>
2022-07-03 22:11:34 +0200

    create category tests

---
+++ tests/category/repository.test.ts
@@ -0,0 +1,16 @@
+import { AuctionLinkRepository } from '../../repository/auction-link.repository';
+import { pool } from '../../utils/db';
+import { CategoryRepository } from '../../repository/category.repository';
+
+afterAll(() => {
+  pool.end();
+});
+
+test('find all', async () => {
+  const category = await CategoryRepository.findAll();
+
+  expect(category).not.toBeNull();
+  expect(category).not.toEqual([]);
+  expect(category.length).toBe(5);
+  expect(category[0].id).toBe('3aed0ace-acfc-4dc1-9eb9-85880ebe9a69');
+});
